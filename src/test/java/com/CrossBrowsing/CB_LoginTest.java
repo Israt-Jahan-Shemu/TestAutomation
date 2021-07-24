7452130a1858b9187d921f1c61bf693cad1b555f
Israt-Jahan-Shemu <isratshimu@gmail.com>
2021-07-25 00:06:24 +0600

    Practice cross browsing using TestNG

---
+++ src/test/java/com/CrossBrowsing/CB_LoginTest.java
@@ -0,0 +1,68 @@
+package com.CrossBrowsing;
+
+import com.Utils_log.LogInstance;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.Assert;
+import org.testng.annotations.*;
+
+public class CB_LoginTest {
+    WebDriver driver;
+    String LogInURL="https://demo.opencart.com/index.php?route=account/login";
+
+    @BeforeClass
+    @Parameters("Browser")
+    public void initSetup(String browserName){
+        if(browserName.equalsIgnoreCase("Firefox")) {
+            System.setProperty("webdriver.gecko.driver", "./src/main/resources/geckodriver.exe");
+            driver = new FirefoxDriver();
+            LogInstance.info("Firefox Launch Successfully.");
+        }
+        else if(browserName.equalsIgnoreCase("Chrome")) {
+            System.setProperty("webdriver.chrome.driver", "./src/main/resources/chromedriver.exe");
+            driver = new ChromeDriver();
+            LogInstance.info("Chrome Launch Successfully.");
+        }
+
+    }
+    @AfterClass
+    public void tearDown() {
+        driver.close();
+        LogInstance.info(" Chrome Close Browser");
+
+    }
+    @Test
+    public void TC001_Valid() {
+        driver.get(LogInURL);
+        LogInstance.info("Login Page Open Successfully.");
+
+        WebElement Email=driver.findElement(By.id("input-email"));
+        Email.sendKeys("user101@gmail.com");
+        LogInstance.info("Type Email");
+
+        //Step 3
+        WebElement Password=driver.findElement(By.id("input-password"));
+        Password.sendKeys("123456");
+        LogInstance.info("Type Password");
+
+        WebElement LoginBtn=driver.findElement(By.xpath("//*[@id=\"content\"]/div/div[2]/div/form/input"));
+        LoginBtn.click();
+        LogInstance.info("Click on Login Button");
+
+        //Logic Develop
+        //Login Pass
+        String Exp_Title="My Account";
+        String Act_Title=driver.getTitle();
+
+        //Assertion
+        Assert.assertEquals(Exp_Title,Act_Title);
+
+        //Logout
+        WebElement logout=driver.findElement(By.linkText("Logout"));
+        logout.click();
+    }
+
+}
